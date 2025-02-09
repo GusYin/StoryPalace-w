c4f816d5fcdf69385f1e5b7598d20190bdc32e94
yini <yiniski@outlook.com>
2025-02-09 16:38:03 +1300

    Remove get started button margin bottom.

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -42,7 +42,7 @@ const SignUpPage = () => {
             <button className="hover:text-blue-600">Login</button>
             <button
               type="button"
-              className="text-white bg-custom-teal hover:bg-teal-700 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mb-0"
+              className="text-white bg-custom-teal hover:bg-teal-700 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
             >
               Get started
             </button>
