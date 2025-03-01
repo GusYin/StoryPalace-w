21026c65dee0c00e35a8b6132312fa7ebed83022
yini <yiniski@outlook.com>
2025-03-02 08:42:44 +1300

    Put microhpone and upload icon into tsx and delete 3rd party icons e.g. apple, google.

--- app/routes/upload-voice.tsx
+++ app/routes/upload-voice.tsx
@@ -3,6 +3,8 @@ import { httpsCallable } from "firebase/functions";
 import { functions } from "~/firebase/firebase";
 import { DeleteIcon } from "~/components/icons/delete-icon";
 import { UploadIcon } from "~/components/icons/upload-icon";
+import { UploadIconLg } from "~/components/icons/upload-icon-lg";
+import { MicrophoneIcon } from "~/components/icons/microphone-icon";
 
 interface VoiceUploadUrlRequest {
   contentType: string;
@@ -286,19 +288,7 @@ const VoiceUploadPage = () => {
               // Upload UI
               <div className="space-y-3">
                 <label htmlFor="file-upload" className="cursor-pointer">
-                  <svg
-                    className="mx-auto h-8 w-8"
-                    width="26"
-                    height="25"
-                    viewBox="0 0 26 25"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M3.36945 25C2.48663 25 1.73088 24.6857 1.1022 24.057C0.473519 23.4283 0.15918 22.6726 0.15918 21.7897V18.5795C0.15918 18.1247 0.313005 17.7434 0.620656 17.4358C0.928308 17.1281 1.30953 16.9743 1.76432 16.9743C2.2191 16.9743 2.60032 17.1281 2.90798 17.4358C3.21563 17.7434 3.36945 18.1247 3.36945 18.5795V21.7897H22.6311V18.5795C22.6311 18.1247 22.7849 17.7434 23.0926 17.4358C23.4002 17.1281 23.7814 16.9743 24.2362 16.9743C24.691 16.9743 25.0722 17.1281 25.3799 17.4358C25.6875 17.7434 25.8414 18.1247 25.8414 18.5795V21.7897C25.8414 22.6726 25.527 23.4283 24.8983 24.057C24.2697 24.6857 23.5139 25 22.6311 25H3.36945ZM11.3951 5.49759L8.3855 8.50722C8.06448 8.82825 7.68326 8.98208 7.24184 8.9687C6.80043 8.95532 6.41921 8.78812 6.09818 8.46709C5.80391 8.14607 5.65008 7.77154 5.63671 7.3435C5.62333 6.91546 5.77716 6.54093 6.09818 6.2199L11.8767 0.441413C12.0372 0.280899 12.2111 0.167202 12.3983 0.100321C12.5856 0.0334403 12.7863 0 13.0003 0C13.2143 0 13.4149 0.0334403 13.6022 0.100321C13.7895 0.167202 13.9634 0.280899 14.1239 0.441413L19.9024 6.2199C20.2234 6.54093 20.3772 6.91546 20.3638 7.3435C20.3505 7.77154 20.1966 8.14607 19.9024 8.46709C19.5813 8.78812 19.2001 8.95532 18.7587 8.9687C18.3173 8.98208 17.9361 8.82825 17.615 8.50722L14.6054 5.49759V16.9743C14.6054 17.4291 14.4516 17.8103 14.1439 18.118C13.8363 18.4256 13.4551 18.5795 13.0003 18.5795C12.5455 18.5795 12.1643 18.4256 11.8566 18.118C11.549 17.8103 11.3951 17.4291 11.3951 16.9743V5.49759Z"
-                      fill="black"
-                    />
-                  </svg>
+                  <UploadIconLg />
                 </label>
 
                 <p className="font-medium text-black">
@@ -328,15 +318,7 @@ const VoiceUploadPage = () => {
                     onClick={() => setShowRecordingUI(true)}
                     className="flex items-center gap-2 py-3 px-6 bg-white hover:bg-gray-200 text-black rounded-full font-medium transition-colors"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="24px"
-                      viewBox="0 -960 960 960"
-                      width="24px"
-                      fill="#5f6368"
-                    >
-                      <path d="M480-400q-50 0-85-35t-35-85v-240q0-50 35-85t85-35q50 0 85 35t35 85v240q0 50-35 85t-85 35Zm0-240Zm-40 520v-123q-104-14-172-93t-68-184h80q0 83 58.5 141.5T480-320q83 0 141.5-58.5T680-520h80q0 105-68 184t-172 93v123h-80Zm40-360q17 0 28.5-11.5T520-520v-240q0-17-11.5-28.5T480-800q-17 0-28.5 11.5T440-760v240q0 17 11.5 28.5T480-480Z" />
-                    </svg>
+                    <MicrophoneIcon />
                     Record audio
                   </button>
                 </div>
