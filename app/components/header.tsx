2dde5867578d4f417247f9a89d774dab69627b49
yini <yiniski@outlook.com>
2025-03-30 22:35:35 +1300

    feat: add SkeletonHeader component and implement loading state in Header for improved user experience

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,19 +2,26 @@ import { useState, useEffect } from "react";
 import AuthHeader from "./auth-header";
 import UnauthHeader from "./unauth-header";
 import { auth } from "~/firebase/firebase";
+import SkeletonHeader from "./skeleton-header";
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     // Listen for authentication state changes
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setIsLoggedIn(user?.emailVerified === true);
+      setIsLoading(false);
     });
 
     // Cleanup subscription on unmount
     return () => unsubscribe();
   }, []);
+
+  if (isLoading) {
+    return <SkeletonHeader />;
+  }
 
   return isLoggedIn ? <AuthHeader /> : <UnauthHeader />;
 };
---
+++ app/components/skeleton-header.tsx
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router";
+import { StoryPalaceLogoWithText } from "./icons/story-palace-logo";
+import { StoryPalaceLogoNoText } from "./icons/story-palace-logo-no-text";
+
+const SkeletonHeader = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      {/* Sticky Navigation Bar */}
+      <nav className="shadow-[0_4px_20px_0_rgba(0,0,0,0.05)] sticky h-[64px] top-0 bg-white shadow-xs z-50 relative font-dosis text-sm font-[700]">
+        {/* Responsive Logo */}
+        {/* Left-aligned Logo */}
+        <button
+          className="cursor-pointer absolute top-1/2 -translate-y-1/2 left-4 md:left-8"
+          onClick={() => navigate("/")}
+        >
+          {/* Mobile logo (visible on small screens) */}
+          <span className="block md:hidden">
+            <StoryPalaceLogoNoText className="h-8 w-auto" />
+          </span>
+
+          {/* Desktop logo (visible on medium screens and up) */}
+          <span className="hidden md:block -ml-8">
+            <StoryPalaceLogoWithText className="h-8 w-auto" />
+          </span>
+        </button>
+
+        {/* Right-aligned Auth Buttons */}
+        <div className="absolute right-8 top-1/2 -translate-y-1/2 flex space-x-4 items-center">
+          <button
+            type="button"
+            className="cursor-pointer text-black bg-grey-bg-lucile hover:bg-teal-700 focus:outline-hidden focus:ring-4 focus:ring-green-300 rounded-[30px] px-5 py-2.5 text-center me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          ></button>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default SkeletonHeader;
